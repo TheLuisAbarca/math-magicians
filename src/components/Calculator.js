a3a941fdc916d5c58e8069965d363ce955e1e19d
TheLuisAbarca <arclaav.adm@gmail.com>
2022-01-05 11:59:08 -0500

    Implement functionality of calculator using the logic located on calculate and operate files.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,29 +1,61 @@
 import React from 'react';
 import { buttons } from './keypadCalculator';
+import calculate from '../logic/calculate';
 import './Calculator.css';
 
 class Calculator extends React.Component {
-  calculatorStructure = () => (
-    <div className="calculator">
-      <div className="calculator-display">
-        <div className="display-value">0</div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '',
+      next: '',
+      operation: '',
+    };
+  }
+
+  pressButtonHandler = (event) => {
+    this.setState((prev) => {
+      try {
+        const output = calculate(prev, event.target.value);
+        return ({
+          ...prev,
+          ...output,
+        });
+      } catch (e) {
+        return prev;
+      }
+    });
+  };
+
+  calculatorStructure = () => {
+    const { total, next, operation } = this.state;
+    const display = (total || '') + (operation || '') + (next || '');
+    return (
+      <div className="calculator">
+        <div className="calculator-display">
+          <div className="display-value">
+            {display || '0'}
+          </div>
+        </div>
+        <div className="calculator-keypad">
+          {buttons.map((button) => {
+            const { text, className } = button;
+            return (
+              <button
+                value={text}
+                key={text}
+                type="button"
+                className={`keypad-button ${className} || ''`}
+                onClick={this.pressButtonHandler}
+              >
+                {text}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <div className="calculator-keypad">
-        {buttons.map((button) => {
-          const { text, className } = button;
-          return (
-            <button
-              type="button"
-              key={text}
-              className={`keypad-button ${className} || ''`}
-            >
-              {text}
-            </button>
-          );
-        })}
-      </div>
-    </div>
-  )
+    );
+  };
 
   render() {
     return (
